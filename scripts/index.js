8cd8201e18b41f3fe808746553d56f942131f7bf
Marie Greengrass <marie.greengrass@gmail.com>
2021-05-27 23:57:43 +0300

    Fix JS, structure, CSS and HTML

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,31 @@
-    let popupWindow = document.querySelector('.popup');
-    let editButton = document.querySelector('.profile__edit-button');
-    let closeButton = document.querySelector('.popup__close-button');
+let popupWindow = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
 
-    editButton.addEventListener('click', () => {
-        popupWindow.classList.add('popup_opened')
-    });
-    closeButton.addEventListener('click', () => {
-        popupWindow.classList.remove('popup_opened')
-    });
+editButton.addEventListener('click', () => {
+    popupWindow.classList.add('popup_opened')
+    nameInput.value = profileHeader.textContent;
+    jobInput.value = profileRank.textContent;
 
-    let form = document.querySelector('.popup__container');
-    let nameInput = document.querySelector('.popup__item_heading');
-    let jobInput = document.querySelector('.popup__item_rank');
-    let profileHeader = document.querySelector('.profile__header');
-    let profileRank = document.querySelector('.profile__rank');
-    
-    function formSubmitHandler (evt) {
-        evt.preventDefault();
-        profileHeader.textContent = nameInput.value;
-        profileRank.textContent = jobInput.value;
-    }
-    form.addEventListener('submit', formSubmitHandler);
-    form.addEventListener('submit', () => {
-        popupWindow.classList.remove('popup_opened')
-    });
+});
+closeButton.addEventListener('click', () => {
+    closePopup();
+
+});
+function closePopup () {
+    popupWindow.classList.remove('popup_opened')
+}
+
+let form = document.querySelector('.popup__form');
+let nameInput = document.querySelector(".popup__item_type_name");
+let jobInput = document.querySelector(".popup__item_type_rank");
+let profileHeader = document.querySelector('.profile__header');
+let profileRank = document.querySelector('.profile__rank');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileHeader.textContent = nameInput.value;
+    profileRank.textContent = jobInput.value;
+    closePopup();
+}
+form.addEventListener('submit', formSubmitHandler);
